57f693024df0806fbf970b98ce3bf4f603f370bf
Anuj S M <Anuj.SM@valtech.com>
2022-09-30 16:15:13 +0530

    :P

---
+++ maven_example/src/main/java/com/valtech/training/maven/example/App.java
@@ -0,0 +1,13 @@
+package com.valtech.training.maven.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
